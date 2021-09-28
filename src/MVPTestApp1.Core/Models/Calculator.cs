636fe599b8dc77f06e47018a6b8be99151b57c50
renes-home-repo <rene.skarabot@epiatech.com>
2021-09-28 21:29:58 +0200

    Version 0.1

---
+++ src/MVPTestApp1.Core/Models/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPTestApp1.Core.Models
+{
+    public class Calculator
+    {
+        public decimal CurrentValue { get; set; }
+    }
+}
---
+++ src/MVPTestApp1/Presenters/CalculatorPresenter.cs
@@ -0,0 +1,19 @@
+﻿using MVPTestApp1.Core.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVPTestApp1.Presenters
+{
+    public class CalculatorPresenter
+    {
+        private readonly ICalculatorView _view;
+        public CalculatorPresenter(ICalculatorView view)
+        {
+            _view = view;
+
+        }
+    }
+}
